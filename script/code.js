3b3559de98afad9b2ccd151ca806c579771eb8a2
LeahBasson <leahbasson54@gmail.com>
2024-05-15 15:21:24 +0200

    Update

--- script/code.js
+++ script/code.js
@@ -5,22 +5,18 @@ console.log(typeof firstName);
 
 // string datatype
 let lastName = "Bassson";
-console.log(lastName);
 console.log(typeof lastName);
 
 //BigInt
 let numb1 = BigInt(9999999999999999999);
-console.log(numb1);
 console.log(typeof numb1);
 
 //Number
 let numb2 = 12;
-console.log(numb2);
 console.log(typeof numb2);
 
 //Boolean
 let answer = Boolean(true);
-console.log(answer);
 console.log(typeof answer);
 
 //Undefined
